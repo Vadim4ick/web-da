1a67f70804c4cac1842c500e675d4825ccb073fd
Vadim Firulyov <firulvv@mail.ru>
2025-09-26 00:37:05 +0400

    ✨ (package.json, route.ts, layout.tsx, BannerSend.tsx, globals.css, Providers.tsx): добавлена интеграция Google reCAPTCHA v3 Добавлена библиотека `react-google-recaptcha-v3` для защиты форм от спама. Реализована проверка токена reCAPTCHA на сервере и клиенте, что повышает безопасность отправки данных. Также создан компонент `Providers` для обертки приложения в провайдер Google reCAPTCHA, что упрощает использование капчи в различных частях приложения.

---
+++ src/shared/providers/Providers.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3'
+
+const Providers = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <GoogleReCaptchaProvider
+      reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY!}
+      scriptProps={{
+        async: true,
+        defer: true,
+        appendTo: 'head',
+      }}
+    >
+      {children}
+    </GoogleReCaptchaProvider>
+  )
+}
+
+export { Providers }
