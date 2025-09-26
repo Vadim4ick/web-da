3036797199e6c27057945db397798c8bca6672d2
Vadim <a-yva-2013@mail.ru>
2025-09-27 00:01:00 +0400

    ✨ (BurgerMenu.tsx, BurgerMenuIcon.tsx): добавлена анимация для иконки бургер-меню 💡 (BurgerMenuIcon.tsx): улучшен код и добавлены комментарии Анимация иконки бургер-меню теперь реализована с помощью библиотеки motion, что улучшает пользовательский интерфейс и делает его более интерактивным. Удален CloseIcon, так как теперь иконка бургер-меню изменяет свое состояние в зависимости от открытости меню. Комментарии добавлены для улучшения читаемости кода.

--- src/shared/icons/BurgerMenuIcon.tsx
+++ src/shared/icons/BurgerMenuIcon.tsx
@@ -1,18 +1,68 @@
-const BurgerMenuIcon = (props: React.SVGProps<SVGSVGElement>) => {
+'use client'
+
+import * as React from 'react'
+import { motion } from 'motion/react'
+type AnimatedBurgerIconProps = React.SVGProps<SVGSVGElement> & {
+  open: boolean
+}
+
+export const BurgerMenuIcon: React.FC<AnimatedBurgerIconProps> = ({ open }) => {
   return (
-    <svg
+    <motion.svg
       width="18"
       height="18"
       viewBox="0 0 18 18"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      {...props}
+      animate={open ? 'open' : 'closed'}
     >
-      <rect x="7" y="13" width="9" height="2" rx="1" fill="#2D3240" />
-      <rect x="2" y="3" width="9" height="2" rx="1" fill="#2D3240" />
-      <rect x="2" y="8" width="14" height="2" rx="1" fill="#2D3240" />
-    </svg>
-  );
-};
+      {/* Верхняя полоса */}
+      <motion.rect
+        x="7"
+        y="13"
+        width="9"
+        height="2"
+        rx="1"
+        fill="#2D3240"
+        initial={false}
+        animate={open ? { rotate: 45, y: -2 } : { rotate: 0, y: 0 }}
+        transition={{
+          duration: 0.4,
+          ease: 'easeOut',
+        }}
+      />
 
-export { BurgerMenuIcon };
+      {/* Центральная полоса — поворачивается «боком» и пропадает */}
+      <motion.rect
+        x="2"
+        y="3"
+        width="9"
+        height="2"
+        rx="1"
+        fill="#2D3240"
+        initial={false}
+        animate={open ? { rotate: 45, y: 2 } : { rotate: 0, y: 0 }}
+        transition={{
+          duration: 0.4,
+          ease: 'easeOut',
+        }}
+      />
+
+      {/* Нижняя полоса */}
+      <motion.rect
+        x="2"
+        y="8"
+        width="14"
+        height="2"
+        rx="1"
+        fill="#2D3240"
+        initial={false}
+        animate={open ? { rotate: -45, width: 18, x: -2 } : { rotate: 0, width: 14, x: 0 }}
+        transition={{
+          duration: 0.4,
+          ease: 'easeOut',
+        }}
+      />
+    </motion.svg>
+  )
+}
